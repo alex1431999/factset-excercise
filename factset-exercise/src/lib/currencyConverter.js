5b1123842282472ba66c9d78a95cd5e7442b819e
alex1431999 <alexhaller99@yahoo.de>
2020-05-06 10:25:43 +0100

    Add airbnb base linting
    Fix all linting errors

--- factset-exercise/src/lib/currencyConverter.js
+++ factset-exercise/src/lib/currencyConverter.js
@@ -5,52 +5,52 @@
 class CurrencyConveter {
   /**
    * Instatiate the currency converter
-   * @param {Array<JSON>} currencies 
+   * @param {Array<JSON>} currencies
    */
   constructor(currencies) {
-    this.currencies = currencies
+    this.currencies = currencies;
   }
 
   /**
    * Convert a currency into another
-   * @param {String} base 
-   * @param {String} target 
-   * @param {Float} value 
+   * @param {String} base
+   * @param {String} target
+   * @param {Float} value
    */
   convert(base, target, value) {
-    const rate = this.getRate(base, target)
+    const rate = this.getRate(base, target);
 
     /* Saveguard */
     if (rate === null) {
-      throw Error('Unsupported currency conversion')
+      throw Error('Unsupported currency conversion');
     }
-    
-    return (value * rate).toFixed(2) // Only show 2 decimals
+
+    return (value * rate).toFixed(2); // Only show 2 decimals
   }
 
   /**
    * Get the conversion rate of two currencies
-   * @param {String} base 
-   * @param {String} target 
+   * @param {String} base
+   * @param {String} target
    */
   getRate(base, target) {
     for (let i = 0; i < this.currencies.length; i += 1) {
-      const currency = this.currencies[i]
+      const currency = this.currencies[i];
 
       /* If the input matches exactly, just return the rate */
       if (currency.base === base && currency.target === target) {
-        return currency.rate
+        return currency.rate;
       }
 
       /* If the input matches in reverse, return the adjusted rate */
       if (currency.base === target && currency.target === base) {
-        return 1 / currency.rate
+        return 1 / currency.rate;
       }
     }
 
     // Currency combination does not exist
-    return null
+    return null;
   }
 }
 
-export default CurrencyConveter
+export default CurrencyConveter;
--- factset-exercise/src/main.js
+++ factset-exercise/src/main.js
@@ -1,13 +1,13 @@
-import '@babel/polyfill'
-import 'mutationobserver-shim'
-import Vue from 'vue'
-import './plugins/bootstrap-vue'
-import App from './App.vue'
-import i18n from './i18n'
+import '@babel/polyfill';
+import 'mutationobserver-shim';
+import Vue from 'vue';
+import './plugins/bootstrap-vue';
+import App from './App.vue';
+import i18n from './i18n';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   i18n,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
